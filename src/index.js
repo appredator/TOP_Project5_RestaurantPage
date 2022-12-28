8bdff0b4bd3e78a0cb011749740a17f1d198f3e4
appredator <volbass92@gmail.com>
2022-12-27 18:08:04 -0600

    Finished Home Page

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import menuPageModule from './menu.js';
   const homePageModule = function(){
     const content = document.querySelector("#content");
 
-    const title = document.createElement("h2");
+    const title = document.createElement("h1");
     title.id="restaurantTitle";
     title.innerHTML = "Fresh Pressed Juice Co."
     content.appendChild(title);
 
     const imageBanner = document.createElement("img");
+    imageBanner.id = "imageBanner"
     content.appendChild(imageBanner)
 
     const buttonDiv = document.createElement("div");
@@ -35,6 +36,30 @@ import menuPageModule from './menu.js';
     buttonThree.innerHTML = "Contact"
     buttonDiv.appendChild(buttonThree)
 
+    const homeDiv = document.createElement("div");
+    homeDiv.id = 'homeDiv'
+    content.appendChild(homeDiv)
+
+    const homeMiddleTitle = document.createElement("h3");
+    homeMiddleTitle.id = 'homeMiddleTitle'
+    homeMiddleTitle.innerHTML = "The most fresh juices and smoothies in town!"
+    homeDiv.appendChild(homeMiddleTitle)
+
+    const homeMiddleText = document.createElement("p");
+    homeMiddleText.id = 'homeMiddleText'
+    homeMiddleText.innerHTML = "We were established in 2005 with the idea that no one would provide more robust and healthy smoothies and juices in the Coffee County Area. We work really hard with our produce and fruit vendors to ensure the ingredients we use on a daily basis are second to none! Come on out and support a local business with the best intentions around."
+    homeDiv.appendChild(homeMiddleText)
+
+    //Add two line breaks to space center image
+    const breakOne = document.createElement("br");
+    homeDiv.appendChild(breakOne)
+    const breakTwo = document.createElement("br");
+    homeDiv.appendChild(breakTwo)
+
+    // Big Middle Image
+    const imageMiddle = document.createElement("img");
+    imageMiddle.id = 'imageMiddle'
+    homeDiv.appendChild(imageMiddle)
 
 }
 
--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,36 @@
 const menuPageModule = function(){
+    
     const content = document.querySelector("#content");
+    
+    buttonDiv.remove();
+    homeDiv.remove();
+    
+    const buttonDivMenu = document.createElement("div");
+    buttonDivMenu.id = 'buttonDiv'
+    content.appendChild(buttonDivMenu)
+
+    const buttonOneMenu = document.createElement("button");
+    buttonOneMenu.id = 'home'
+    buttonOneMenu.className = 'buttonBar'
+    buttonOneMenu.innerHTML = "Home"
+    buttonDiv.appendChild(buttonOneMenu)
+
+    const buttonTwo = document.createElement("button");
+    buttonTwo.id = 'menu'
+    buttonTwo.className = 'buttonBar'
+    buttonTwo.innerHTML = "Menu"
+    buttonDiv.appendChild(buttonTwo)
+
+    const buttonThree = document.createElement("button");
+    buttonThree.id = 'menu'
+    buttonThree.className = 'buttonBar'
+    buttonThree.innerHTML = "Contact"
+    buttonDiv.appendChild(buttonThree)
 
     const menuBox = document.createElement("div");
     menuBox.id="menuBox";
     menuBox.innerHTML = "Menu Item 1"
     content.appendChild(menuBox);
-
-
-
-    // const test = document.createElement("span");
-    // test.innerHTML = "TEST TEST"
-    // content.appendChild(test)
-
 
 }
 
