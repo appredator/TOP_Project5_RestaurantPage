a38b8d7eef7c04cdd54267ac79f9a7676994b275
appredator <volbass92@gmail.com>
2022-12-27 18:49:54 -0600

    Menu Page Items 2

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,5 @@
 const menuPageModule = function(){
-    
+
     const content = document.querySelector("#content");
     
     buttonDiv.remove();
@@ -27,10 +27,49 @@ const menuPageModule = function(){
     buttonThree.innerHTML = "Contact"
     buttonDiv.appendChild(buttonThree)
 
+    // Menu Box 1
     const menuBox = document.createElement("div");
     menuBox.id="menuBox";
-    menuBox.innerHTML = "Menu Item 1"
     content.appendChild(menuBox);
+
+    const menuItemHeaderOne = document.createElement("h3");
+    menuItemHeaderOne.id="menuItemHeader";
+    menuItemHeaderOne.innerHTML = "Green Machine"
+    menuBox.appendChild(menuItemHeaderOne);
+
+
+    const ingredients = document.createElement("span");
+    ingredients.id="menuItemText";
+    ingredients.innerHTML = "Ingredients:"
+    menuBox.appendChild(ingredients);
+
+    const menuItemText = document.createElement("p");
+    menuItemText.id="menuItemText";
+    menuItemText.innerHTML = "Lime, Lemon, Ginger, Spinach, Kale, and Pineapple"
+    menuBox.appendChild(menuItemText);
+
+     // Menu Box 2
+     const menuBox2 = document.createElement("div");
+     menuBox2.id="menuBox";
+     content.appendChild(menuBox2);
+ 
+     const menuItemHeaderTwo = document.createElement("h3");
+     menuItemHeaderTwo.id="menuItemHeader";
+     menuItemHeaderTwo.innerHTML = "Peach Fuzzy"
+     menuBox2.appendChild(menuItemHeaderTwo);
+ 
+ 
+     const ingredientsTwo = document.createElement("span");
+     ingredientsTwo.id="menuItemText";
+     ingredientsTwo.innerHTML = "Ingredients:"
+     menuBox2.appendChild(ingredientsTwo);
+ 
+     const menuItemTextTwo = document.createElement("p");
+     menuItemTextTwo.id="menuItemText";
+     menuItemTextTwo.innerHTML = "Apple, Orange, Ginger, Spinach, Kale, and Pineapple"
+     menuBox2.appendChild(menuItemTextTwo);
+
+    
 
 }
 
